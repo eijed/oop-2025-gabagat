e4d9c0cef30423566d3803fe204d29384eebb328
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-03-29 08:47:07 +0800

    string2 and quiz

---
+++ 008_quiz/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void Main(String[] args) {
+    int num1 = 8709.222;
+    int num2 = 2020.244;
+
+    // Display the sum of `num1` and `num2` in 2 decimal places: 10729.47
+    System.out.println(String.format("{??}", (num1 + num2)));
+  }
+}

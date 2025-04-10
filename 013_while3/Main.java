ccc35e2ad20c680642515a1dd2693f5ced3acc9e
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-04-10 21:50:26 +0800

    feat: added more while exercises

---
+++ 013_while3/Main.java
@@ -0,0 +1,14 @@
+// `break` statement is used to jump out of a loop
+
+public class Main {
+  public static void main(String[] args) {
+    int num = 40;
+    // Given 40 to 1. Stop the countdown once
+    // the current number is divisible by 15.
+    while (??) {
+      num??;
+      if (num % 15 == 0) ??;
+      System.out.println(num);
+    }
+  }
+}

2492a03ca0be7b34637f095012505d231a16083d
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-04-13 21:52:35 +0800

    restructure

--- check.sh
+++ check.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-cd ./$1;
+cd $1;
 rm -rf ./*.class;
-javac -cp .:../lib/junit-platform-console-standalone-1.9.3.jar Main.java MainTest.java;
-java -cp .:../lib/junit-platform-console-standalone-1.9.3.jar org.junit.runner.JUnitCore MainTest;
+javac -cp .:../../lib/junit-platform-console-standalone-1.9.3.jar Main.java MainTest.java;
+java Main
+java -cp .:../../lib/junit-platform-console-standalone-1.9.3.jar org.junit.runner.JUnitCore MainTest;
+
+#if [ $? -eq 0 ]; then
+#  echo "OK"
+#else
+#  echo "Failed Test"
+#fi
+
 rm -rf ./*.class;
-cd ../;
+cd ../../;

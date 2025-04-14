d703f5f0fe44262d663a8b8d309c59b07034fc56
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-04-14 21:23:24 +0800

    for loop (iter)

---
+++ basics/015_for2/Main.java
@@ -0,0 +1,17 @@
+// `for` is also used to cycle through elements in an iteratable.
+//
+//        String[] fruits = {"Apple", "Banana", "Cherry"};
+//
+//        for (String fruit : fruits) {
+//            System.out.println(fruit);
+//        }
+
+public class Main {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry"};
+
+        for (??? : ???) {
+            System.out.println(fruit);
+        }
+    }
+}

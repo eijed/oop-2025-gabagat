ab4ee4227379ea1596d977083cad4f59f09b5ab6
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-04-18 21:43:48 +0800

    support packages

--- check.sh
+++ check.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
+
+if [ -z "$1" ]; then
+  echo "path required.";
+  exit 1;
+fi
+
 cd $1;
-rm -rf ./*.class;
-javac -cp .:../../lib/junit-platform-console-standalone-1.9.3.jar Main.java MainTest.java;
-java Main
+rm -rf ./*.class ./**/*.class;
+
+export JAV_FILES="*.java"
+
+if find . -mindepth 1 -maxdepth 1 -type d  | grep -q .; then
+  export JAV_FILES="*.java **/*.java"
+fi
+
+javac -cp .:../../lib/junit-platform-console-standalone-1.9.3.jar $JAV_FILES;
+java Main;
 java -cp .:../../lib/junit-platform-console-standalone-1.9.3.jar org.junit.runner.JUnitCore MainTest;
 
 #if [ $? -eq 0 ]; then
@@ -11,5 +24,5 @@ java -cp .:../../lib/junit-platform-console-standalone-1.9.3.jar org.junit.runne
 #  echo "Failed Test"
 #fi
 
-rm -rf ./*.class;
+rm -rf ./*.class ./**/*.class;
 cd ../../;

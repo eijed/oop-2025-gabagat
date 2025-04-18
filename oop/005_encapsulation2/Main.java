231feb6f84a324a2a40c364920ee5d56f92e7c20
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-04-18 21:40:51 +0800

    public, protected, and private

---
+++ oop/005_encapsulation2/Main.java
@@ -0,0 +1,20 @@
+// Now that we know what's the idea of encapsulation.
+// Let us explore these access modifiers starting with `public` modifer
+public class Main {
+    public static void main(String[] args) {
+        Car civic = new Car();
+        // Assign make and model values to our object
+        civic.make = "Honda";
+        civic.model = "Civic";
+    }
+}
+
+class Car {j
+    public String make;
+    // apply the correct modifer
+    ??? String model;
+
+    public Car() {
+        System.out.println("A car is created.");
+    }
+}

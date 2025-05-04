fb0d39f5c6021b81748ee3581af638ad1a5c2b58
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-05-04 20:43:14 +0800

    scene2 and textfield

--- javafx/001-scene/src/main/java/com/cmi/App.java
+++ javafx/001-scene/src/main/java/com/cmi/App.java
@@ -1,5 +1,15 @@
 package com.cmi;
 
+/*
+ * A scene graph is a tree data structure that arranges (and groups) graphical objects for easier logical representation.
+ * It also allows the graphics engine to render the objects in the most efficient way by fully or partially skipping objects 
+ * which will not be seen in the final image.
+ * 
+ * A stage is a JavaFX representation of a native OS window. At any given time a stage can have a single Scene attached to it. 
+ * 
+ * https://fxdocs.github.io/docs/html5/#_overview
+ */
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
